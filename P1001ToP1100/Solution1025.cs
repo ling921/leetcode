b51a7e9c95ca56b10bef065afd510da46fcc8ee2
LingDev <lingjing0921@live.com>
2021-02-01 15:28:53 +0800

    Add Solution 1025

---
+++ P1001ToP1100/Solution1025.cs
@@ -0,0 +1,14 @@
+﻿namespace P1001ToP1100
+{
+    public class Solution1025
+    {
+        public Solution1025()
+        {
+        }
+
+        public bool DivisorGame(int N)
+        {
+            return N % 2 == 0;
+        }
+    }
+}
