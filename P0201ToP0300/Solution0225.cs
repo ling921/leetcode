b742efc42c835ae4e147ecd6b4d814c1e162dff0
LingDev <lingjing0921@live.com>
2021-02-01 11:57:30 +0800

    Add Solution 225

---
+++ P0201ToP0300/Solution0225.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+
+namespace P0201ToP0300
+{
+    /// <summary>
+    /// 225. 用队列实现栈
+    /// <para>使用队列实现栈的下列操作：</para>
+    /// <code>push(x) -- 元素 x 入栈</code>
+    /// <code>pop() -- 移除栈顶元素</code>
+    /// <code>top() -- 获取栈顶元素</code>
+    /// <code>empty() -- 返回栈是否为空</code>
+    /// </summary>
+    public class Solution0225
+    {
+        public Solution0225()
+        {
+        }
+
+        public class MyStack
+        {
+            private Queue<int> _quenen;
+
+            /** Initialize your data structure here. */
+
+            public MyStack()
+            {
+                _quenen = new Queue<int>();
+            }
+
+            /** Push element x onto stack. */
+
+            public void Push(int x)
+            {
+                _quenen.Enqueue(x);
+            }
+
+            /** Removes the element on top of the stack and returns that element. */
+
+            public int Pop()
+            {
+                var arr = _quenen.ToArray();
+                _quenen.Clear();
+                for (int i = 0; i < arr.Length - 1; i++)
+                {
+                    _quenen.Enqueue(arr[i]);
+                }
+                return arr[^1];
+            }
+
+            /** Get the top element. */
+
+            public int Top()
+            {
+                var arr = _quenen.ToArray();
+                _quenen.Clear();
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    _quenen.Enqueue(arr[i]);
+                }
+                return arr[^1];
+            }
+
+            /** Returns whether the stack is empty. */
+
+            public bool Empty()
+            {
+                return _quenen.Count == 0;
+            }
+        }
+    }
+}
