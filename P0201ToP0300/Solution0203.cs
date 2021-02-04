f7283cc7aad039dfabe6917107466aec98098fde
LingDev <lingjing0921@live.com>
2021-02-04 17:32:12 +0800

    Add Solution 203

---
+++ P0201ToP0300/Solution0203.cs
@@ -0,0 +1,28 @@
+﻿using CommonLibrary.DataStructures;
+
+namespace P0201ToP0300
+{
+    public class Solution0203
+    {
+        public ListNode RemoveElements(ListNode head, int val)
+        {
+            ListNode first = null, pre = null;
+            while (head != null)
+            {
+                if (head.val == val)
+                {
+                    if (pre != null)
+                        pre.next = head.next;
+                }
+                else
+                {
+                    if (first == null)
+                        first = head;
+                    pre = head;
+                }
+                head = head.next;
+            }
+            return first;
+        }
+    }
+}
