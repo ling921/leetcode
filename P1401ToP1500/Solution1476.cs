a33168cbef666994f8c0f9bd4c0e1512a68c4b35
LingDev <lingjing0921@live.com>
2021-02-04 17:00:24 +0800

    Add new Solution to 1476

--- P1401ToP1500/Solution1476.cs
+++ P1401ToP1500/Solution1476.cs
@@ -1,4 +1,6 @@
-﻿namespace P1401ToP1500
+﻿using System.Collections.Generic;
+
+namespace P1401ToP1500
 {
     /// <summary>
     /// 1476. 子矩形查询
@@ -12,8 +14,38 @@
         public class SubrectangleQueries
         {
             private readonly int[][] _value;
+            private readonly List<int[]> _history;
 
             public SubrectangleQueries(int[][] rectangle)
+            {
+                _value = rectangle;
+                _history = new List<int[]>();
+            }
+
+            public void UpdateSubrectangle(int row1, int col1, int row2, int col2, int newValue)
+            {
+                _history.Add(new[] { row1, row2, col1, col2, newValue });
+            }
+
+            public int GetValue(int row, int col)
+            {
+                if (_history.Count > 0)
+                {
+                    for (int i = _history.Count - 1; i >= 0; i--)
+                    {
+                        if (_history[i][0] <= row && row <= _history[i][1] && _history[i][2] <= col && col <= _history[i][3])
+                            return _history[i][4];
+                    }
+                }
+                return _value[row][col];
+            }
+        }
+
+        public class SubrectangleQueries1
+        {
+            private readonly int[][] _value;
+
+            public SubrectangleQueries1(int[][] rectangle)
             {
                 _value = rectangle;
             }
