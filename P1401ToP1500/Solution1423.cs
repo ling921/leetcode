66336d48237e29e10b6b59e8a9c6086b4aeb5d06
Ling <lingjing0921@live.com>
2021-02-06 11:48:52 +0800

    Add Solution 1423

---
+++ P1401ToP1500/Solution1423.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace P1401ToP1500
+{
+    public class Solution1423
+    {
+        public Solution1423()
+        {
+        }
+
+        public int MaxScore(int[] cardPoints, int k)
+        {
+            var sum = 0;
+            for (int i = 0; i < k; i++)
+            {
+                sum += cardPoints[i];
+            }
+            var max = sum;
+            for (int i = 0; i < k; i++)
+            {
+                sum += cardPoints[cardPoints.Length - i - 1] - cardPoints[k - i - 1];
+                max = Math.Max(max, sum);
+            }
+            return max;
+        }
+    }
+}
